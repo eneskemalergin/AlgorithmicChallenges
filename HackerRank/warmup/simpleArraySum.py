a95be3d068f33b37795def1da459ac608e9a1538
eneskemalergin <eneskemalergin@gmail.com>
2016-05-09 17:36:07 -0500

    simple array sum from warmup

---
+++ HackerRank/warmup/simpleArraySum.py
@@ -0,0 +1,10 @@
+# Author: Enes Kemal Ergin
+# Date: 05/09/16
+# https://www.hackerrank.com/challenges/simple-array-sum
+
+
+a = int(raw_input())
+b = raw_input()
+num_b = [int(i) for i in b.split(" ")]
+
+print sum(num_b)
