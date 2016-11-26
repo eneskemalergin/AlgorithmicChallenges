2f42c0032acef3b117a2f215ee37e8cf7a0c7cb3
eneskemalergin <eneskemalergin@gmail.com>
2016-11-26 14:37:49 -0600

    create newYearChaos.py

---
+++ HackerRank/constructiveAlgorithms/newYearChaos.py
@@ -0,0 +1,29 @@
+# Author: Enes Kemal Ergin
+# Date: 05/16/16
+# https://www.hackerrank.com/challenges/new-year-chaos
+
+import sys
+
+
+T = int(raw_input().strip())
+for a0 in xrange(T):
+    n = int(raw_input().strip())
+    q = map(int,raw_input().strip().split(' '))
+    b = {}
+    r = 0
+    cont = True
+    while cont:
+        cont = False
+        for i in xrange(n-1):
+            if q[i] > q[i+1]:
+                if not q[i] in b:
+                    b[q[i]] = 0
+                b[q[i]] += 1
+                if b[q[i]] > 2:
+                    cont = False
+                    r = "Too chaotic"
+                    break
+                q[i], q[i+1] = q[i+1], q[i]
+                r += 1
+                cont = True
+    print r
